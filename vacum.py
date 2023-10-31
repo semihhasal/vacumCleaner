568c41a1e6f2d147f7e9777120df71c5d9fdacdc
Semihhasal <semih.hasal@htomail.com>
2023-10-31 15:46:24 +0300

    add txt files

--- vacum.py
+++ vacum.py
@@ -4,20 +4,21 @@ RoomA = "D"
 RoomB = "D"
 RoomC = "D"
 
-fileA = open('a.txt','w')
-fileB = open('b.txt','w')
+fileA = open('C:/Users/Semih/PycharmProjects/vacumCleaner/a.txt', 'w')
+fileB = open('C:/Users/Semih/PycharmProjects/vacumCleaner/b.txt', 'w')
 
-pA=0.3
-pB=0.3
-pC=0.3
+pA = 0.3
+pB = 0.3
+pC = 0.3
 
-Probility= random.random()
+Probility = random.random()
 
-count=0;
+count = 0;
 print(Probility)
 
+fileA.write('B,D,D,D')
 
+fileA.close()
 
-while(count<1000):
-
- count+1
+while (count < 1000):
+    count = count + 1
